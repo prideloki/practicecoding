90e636cbc06223f92d1e038c5e0dfa15c203e839
/Top <loki.top11@gmail.com>
2014-01-28 22:00:13 +0700

    SRM 159 DIV2 added 3 specific case S-S S-B S--B
    but it does not work

--- TopCoder/src/StreetParking.java
+++ TopCoder/src/StreetParking.java
@@ -9,16 +9,25 @@ public class StreetParking {
 				if(i<2){
 					if(street.charAt(i-1)=='-')count-=2;
 					if(street.charAt(i-1)!='-')count-=1;
+					
 				}else if(i>=2){
-					if(street.charAt(i-1)=='-'&&street.charAt(i-2)=='-')count-=3;
+					if(i>=3&&street.charAt(i-3)=='S'&&street.charAt(i-2)=='-'&&street.charAt(i-1)=='-')count-=4;
+					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)=='S')count-=3;
+					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)=='-')count-=3;
 					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)!='-')count-=2;
 					else if(street.charAt(i-1)!='-'&&street.charAt(i-2)!='-')count-=1;
+					
+					
 				}
+					
+				
 			}else if(token=='S'){
 				if(i==0)count-=1;
 				else if(i>0){
-					if(street.charAt(i-1)=='-')count-=2;
-					if(street.charAt(i-1)!='-')count-=1;
+					if(i>=2&&street.charAt(i-1)=='-'&&street.charAt(i-2)=='S')count-=3;
+					else if(street.charAt(i-1)=='-')count-=2;
+					else if(street.charAt(i-1)!='-')count-=1;
+					
 				}
 			}else if(token=='D'){
 				count-=1;
@@ -36,6 +45,7 @@ public class StreetParking {
 		for(int i=0;i<street.length;i++){
 			System.out.println(freeParks(street[i]));
 		}
+		
 	}
 
 }
