6f27fcaf25e7dfeb40fbe5dcd55db265e683d187
/Top <loki.top11@gmail.com>
2014-01-29 22:28:47 +0700

    SRM 159 DIV 2 passed all test cases might redo it again.

--- TopCoder/src/StreetParking.java
+++ TopCoder/src/StreetParking.java
@@ -1,4 +1,3 @@
-
 public class StreetParking {
 	public static int freeParks(String street){
 		int count=0;
@@ -10,11 +9,16 @@ public class StreetParking {
 					if(street.charAt(i-1)=='-')count-=2;
 					if(street.charAt(i-1)!='-')count-=1;
 					
+					
 				}else if(i>=2){
-					if(i>=3&&street.charAt(i-3)=='S'&&street.charAt(i-2)=='-'&&street.charAt(i-1)=='-')count-=4;
-					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)=='S')count-=3;
-					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)=='-')count-=3;
-					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)!='-')count-=2;
+					if(street.charAt(i-1)=='-'&&street.charAt(i-2)=='-'){
+						if(i>=3&&street.charAt(i-3)=='S')count-=2;
+						else count-=3;
+					}
+					else if(street.charAt(i-1)=='-'&&street.charAt(i-2)!='-'){
+						if(street.charAt(i-2)=='S')count-=1;
+						else count-=2; 
+					}
 					else if(street.charAt(i-1)!='-'&&street.charAt(i-2)!='-')count-=1;
 					
 					
@@ -24,10 +28,11 @@ public class StreetParking {
 			}else if(token=='S'){
 				if(i==0)count-=1;
 				else if(i>0){
-					if(i>=2&&street.charAt(i-1)=='-'&&street.charAt(i-2)=='S')count-=3;
-					else if(street.charAt(i-1)=='-')count-=2;
+					if(i>=2&&street.charAt(i-1)=='-'&&street.charAt(i-2)=='S'){
+						count-=1;
+					}else{if(street.charAt(i-1)=='-')count-=2;
 					else if(street.charAt(i-1)!='-')count-=1;
-					
+					}
 				}
 			}else if(token=='D'){
 				count-=1;
@@ -40,12 +45,18 @@ public class StreetParking {
 		if(count<0)return 0;
 		return count;
 	}
+
 	public static void main(String[] args) {
-		String[] street={"---B--S-D--S--","DDBDDBDDBDD","--S--S--S--S--","SSD-B---BD-DDSB-----S-S--------S-B----BSB-S--B-S-D"};
-		for(int i=0;i<street.length;i++){
-			System.out.println(freeParks(street[i]));
-		}
-		
+		String[] street = { "---B--S-D--S--", "DDBDDBDDBDD", "--S--S--S--S--",
+				"SSD-B---BD-DDSB-----S-S--------S-B----BSB-S--B-S-D" };
+		String test = "--S-B----BSB-S--B-S-D";
+		/*
+		 * for(int i=0;i<street.length;i++){
+		 * System.out.println(freeParks(street[i])); }
+		 */
+		System.out.println(freeParks(street[3]));
+		System.out.println(freeParks("S--B"));
+
 	}
 
 }
