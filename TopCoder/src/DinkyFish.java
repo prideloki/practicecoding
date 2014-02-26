41bc08a3e6adb51da33017e8c7dada33c6be1425
prideloki <loki.top11@gmail.com>
2014-02-26 22:35:01 +0700

    SRM 180 DIV2 waiting for testing

--- TopCoder/src/DinkyFish.java
+++ TopCoder/src/DinkyFish.java
@@ -1,43 +1,24 @@
 
 public class DinkyFish {
-	/*public static int monthsUntilCrowded(int tankVolume, int maleNum, int femaleNum) {
-		int couples = Math.min(maleNum, femaleNum);
-		int rem = (maleNum + femaleNum - 2 * couples);
-		int months = -1;
-		while ((couples + rem+1)/2 <= tankVolume){
-			couples *= 2;
-			months++;
-		}
-		return Math.max(0,months);
-	}*/
+	//need to check for example
 	public static int monthsUntilCrowded(int tankVolume, int maleNum, int femaleNum){
-		
-		return 0;
-		
-	}
-/*	public static int monthsUntilCrowded(int tankVolume, int maleNum, int femaleNum){
-		int limit=tankVolume*2;
-		double numOfFish=maleNum+femaleNum;
-		if(numOfFish==limit)return 1;
-		if(numOfFish>limit)return 0;
-		
-		int count=0;
-		while(numOfFish<limit){
-			
-			numOfFish=Math.pow(2,Math.min(maleNum, femaleNum)+count);
-			
-			count++;
+		int couple =(maleNum<=femaleNum)?maleNum:femaleNum;
+		int lonelyfish=(maleNum+femaleNum)-couple*2;
+		int month=-1;
+		while((couple+lonelyfish+1)/2<=tankVolume){
+			couple*=2;
+			month++;
 		}
-		return count;
-	}*/
+		return month;
+	}
 	public static void main(String[] args) {
 		int[] tankVolume={10,100,5,4,1000000,431131};
 		int[] maleNum={4,4,6,6,3,764};
 		int[] femaleNum={6,6,4,4,2,249};
-		System.out.println(monthsUntilCrowded(431131,764,249));
-		/*for(int i=0;i<tankVolume.length;i++){
+		//System.out.println(monthsUntilCrowded(431131,764,249));
+		for(int i=0;i<tankVolume.length;i++){
 			System.out.println(monthsUntilCrowded(tankVolume[i], maleNum[i], femaleNum[i]));
-		}*/
+		}
 	}
 
 }
