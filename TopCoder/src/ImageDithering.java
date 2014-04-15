8a71c5990b22367876851f055e55aa606339d62e
/Top <loki.top11@gmail.com>
2014-04-15 18:48:34 +0700

    SRM 208 DIV2 250pts

--- TopCoder/src/ImageDithering.java
+++ TopCoder/src/ImageDithering.java
@@ -1,21 +1,14 @@
-
 public class ImageDithering {
-	public static int count(String dithered,String[] screen){
+
+	public int count(String dithered, String[] screen) {
 		int count=0;
-		for(int i=0;i<screen.length;i++){
+		for(int i=0; i<screen.length;i++){
 			for(int j=0;j<screen[i].length();j++){
-				if(dithered.contains(Character.toString(screen[i].charAt(j))))count++;
+				char pixel=screen[i].charAt(j);
+				if(dithered.indexOf(pixel)>=0)count++;
 			}
 		}
 		return count;
 	}
-}	
-/*	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] screen={"ACBD"};
-		String dithered="DCBA";
-		System.out.println(count(dithered, screen));
-	}
 
 }
-*/
