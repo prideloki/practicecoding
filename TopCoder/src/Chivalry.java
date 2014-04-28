4fde0f5adaf2046f2560c2551c1c075adea9d487
prideloki <loki.top11@gmail.com>
2014-04-28 08:43:42 +0700

    SRM 213 DIV2 250pts Try other solution

--- TopCoder/src/Chivalry.java
+++ TopCoder/src/Chivalry.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Chivalry {
 
-	public String getOrder(String first, String second) {
+	public String getOrder2(String first, String second) {
 		String ordered="";
 
 		while(first.length()>0&&second.length()>0){
@@ -26,4 +29,38 @@ public class Chivalry {
 		return ordered;
 	}
 	//TODO tried polphit method
+	public String getOrder(String first, String second) {
+		List<Character> ordered=new ArrayList<Character>();
+		int i=0;
+		int j=0;
+		while(i<first.length()&&j<second.length()){
+			char f=first.charAt(i);
+			char s=second.charAt(j);
+			if(f==s){
+				ordered.add(f);
+				i++;
+			}else{
+				if(f=='W'){
+					ordered.add(f);
+					i++;
+				}else{
+					ordered.add(s);
+					j++;
+				}
+			}
+		}
+		while(i<first.length()){
+			ordered.add(first.charAt(i));
+			i++;
+		}
+		while(j<second.length()){
+			ordered.add(second.charAt(j));
+			j++;
+		}
+		StringBuilder stringBuilder=new StringBuilder(ordered.size());
+		for(int k=0;k<ordered.size();k++){
+			stringBuilder.append(ordered.get(k));
+		}
+		return stringBuilder.toString();
+	}
 }
